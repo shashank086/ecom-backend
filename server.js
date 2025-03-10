11351e085b73d6466bea6846b3933396ee6ad6ca
shashank086 <lokeshshashank19@gmail.com>
2025-03-10 16:15:26 +0530

    third

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const cors=require('cors')
 const connectDB=require("./config/db")
 const app =express()
 
-app.use(express.json())
-app.use(cors())
+const allowedOrigins=[
+    "https://ecom-frontend-indol.vercel.app/",
+    "https://vercel.com/shashank-ls-projects-5fbee625/ecom-frontend/DnBbGr6ugRurqNjZj6RNG3yKqJXg"
+];
 
+app.use(express.json())
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin))
+       {
+        callback(null, true);
+      } 
+      else 
+      {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allows cookies and authentication headers if needed
+  }))
+
+  
 connectDB()
 
 app.use("/auth",require("./routes/authRoutes"))
