36667059d05ed709aff2f2cc10bf80cac37b55c2
shashank086 <lokeshshashank19@gmail.com>
2025-03-10 11:56:18 +0530

    second

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -2,21 +2,21 @@ const { json } = require("express")
 const Cart=require("../models/Cart")
 
 exports.addToCart=async(req,res)=>{
-    const{productID,name,price}=req.body
-    if(!productID){
-        return res.status(400).json({message:"ProductID is missing"})
+    const{productId,name,price}=req.body
+    if(!productId){
+        return res.status(400).json({message:"productId is missing"})
     }
     let cart=await Cart.findOne({userID:req.user.id})
     if(!cart){
         cart=new Cart({userID:req.user.id,item:[]})
     }
-    const existingItem=cart.items.find((item)=>item.productID && item.productID.toString() === productID.toString())
+    const existingItem=cart.items.find((item)=>item.productId && item.productId.toString() === productId.toString())
 
     if(existingItem){
         existingItem.quantity +=1
     }
     else{
-        cart.items.push({productID,name,price,quantity:1})
+        cart.items.push({productId,name,price,quantity:1})
     }
     await cart.save()
     res.json({cart,message:"Item added to cart"})
@@ -28,11 +28,11 @@ exports.getCart=async(req,res)=>{
 }
 
 exports.removeFromCart=async(req,res)=>{
-    const {productID}=req.body
+    const {productId}=req.body
     let cart=await Cart.findOne({userID:req.user.id})
     if(!cart) return res.status(400).json({message:"Cart not found"})
 
-    cart.items =cart.items.filter((item)=>item.productID !== productID)
+    cart.items =cart.items.filter((item)=>item.productId !== productId)
     await cart.save()
     res.json({cart,message:"Item removed from cart"})
 }
